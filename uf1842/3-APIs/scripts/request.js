0bb58dd65dac7ea3c8ae2910f357c20830735b64
dnlslcd <danielsalcedoq@gmail.com>
2024-06-08 12:30:19 +0200

    42 - 3 empezado

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,13 @@
 async function getPuzzle(wordCount) {
     let hiddenSentence = "Buena Suerte";
 
+    const response = await fetch("https://puzzle.mead.io/puzzle?wordCount=1");
+    const data = await response.json;
     return hiddenSentence;
 }
+
+
+// 1. Implementa una llamada a la API para que cada vez que se inicia el juego, se obtenga una palabra aletória
+
+// 2. Fíjate que la función getPuzzle tiene un parámetro, que indica el número de palabras a obtener. Usa el valor de dicho parámetro 
+   // para hacer la petición a la API con el número de palabras deseados.
